5c538f63c76028886cdff638611ae4cbbe8fa91d
KubraCakir <33658135+KubraCakir@users.noreply.github.com>
2017-11-14 20:41:05 +0300

    Add files via upload

---
+++ lightbot.py
@@ -0,0 +1,59 @@
+height = [ [0, 1, 1, 0], [0, 1, 1, 0], [0, 1, 1, 0],
+           [0, 1, 1, 0], [0, 1, 1, 0] ]
+isBlue = [ [True, False, False, False], [False, False, False, False], 
+           [False, False, False, False], [False, False, False, False], 
+           [False, False, False, True] ]
+isOn = [ [False, False, False, False], [False, False, False, False],
+         [False, False, False, False], [False, False, False, False], 
+         [False, False, False, True] ]
+x = 0
+y = 0
+yon = 0
+direction = { 0:'north', 1:'east', 2:'south', 3:'west' }
+
+maxX = len(height) - 1
+maxY = len(height[0]) - 1
+
+
+
+komut = ""
+while komut != "q":
+    print("Enter a command for lbot")
+    komut = input()
+    
+    if komut == '>':
+        print("I am turning right")
+        yon = (yon+1)%4
+        
+    elif komut == '<':
+        print("I am turning left")
+        yon = (yon-1)%4
+        
+    elif komut == "^":
+        if yon == 0:
+            if y < maxY:
+                y = y + 1
+        if yon == 1:
+            if x < maxX:
+                x = x + 1
+        if yon == 2:
+            if y > 0:
+                y = y - 1
+        if yon == 3:
+            if x > 0:
+                x = x - 1
+                
+    elif komut == "@":
+        if ( isBlue[x][y] == True ):
+            print("I am switching on or off")
+            if ( isOn[x][y] == True ):
+                isOn[x][y] = False
+            else:
+                isOn[x][y] = True
+        else:
+            print("You are trying to light up a gray box. I can't do it")
+            
+    elif komut != "q":
+        print("This command is not known")
+        
+print("As I exit now, my orientation is", direction[yon])        
